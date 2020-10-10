b4f5ab5a628dfb9825c4805513f868db9f6fb9bb
GloryNwa <glorynwa@gmail.com>
2020-10-10 15:01:39 +0100

    edited db file

--- admin/db.php
+++ admin/db.php
@@ -6,10 +6,16 @@
 // $db_password = '';
 // $db_name = 'frontline';
 
+// $db_host = 'remotemysql.com';
+// $db_user = 'pK973dx4qo';
+// $db_password ='MZhMl0exwd';
+// $db_name ='pK973dx4qo';
+
+
 $db_host = 'remotemysql.com';
-$db_user = 'pK973dx4qo';
-$db_password ='MZhMl0exwd';
-$db_name ='pK973dx4qo';
+$db_user = '8r7AM7O1lW';
+$db_password ='Kmf4Vyranm';
+$db_name ='8r7AM7O1lW';
 
 $connection = mysqli_connect($db_host,$db_user,$db_password, $db_name);
 	if($connection){

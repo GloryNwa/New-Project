e4b6461e23fd9ebfa8685ddc064188009fdf1d09
GloryNwa <glorynwa@gmail.com>
2020-10-10 14:19:22 +0100

    changing db credntials

--- admin/db.php
+++ admin/db.php
@@ -7,9 +7,9 @@ function db_connect(){
 // $db_name = 'frontline';
 
 $db_host = 'remotemysql.com';
-$db_user = 'zQy2sPP2Hk';
-$db_password = 'rT7dQLRxhv';
-$db_name = 'zQy2sPP2Hk';
+$db_user = 'pK973dx4qo';
+$db_password ='MZhMl0exwd';
+$db_name ='pK973dx4qo';
 
 	$connection = new mysqli($db_host,$db_user,$db_password, $db_name);
 	if($connection->error){
